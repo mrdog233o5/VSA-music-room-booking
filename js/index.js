5daf54e5ea44ca64e315e5b46b43bb33c1fd9a66
mrdog233o5 <mr.dog233o5@gmail.com>
2023-10-16 23:02:21 +0800

    added sound when click

--- js/index.js
+++ js/index.js
@@ -1,25 +1,39 @@
-
-function roomList() {
+async function getText(file) {
+    var myObject = await fetch(file);
+    var myText = await myObject.text();
+    return myText;
+  }
+  
+  async function roomList() {
     var finalCode = "";
-    var container = document.getElementsByClassName('roomsSec')[0];
-    const roomCode = `
-    <div class="room 1 key">
-        <img class="background">
-        <div class="text">
-            <h1 class="left text">{num}</h1>
-            <div class="right text">
-                <div class="placeholder">
-                    <h2 class="location">room: {name}</h2>
-                    <h2 class="teacher">teachers: {teachers}</h2>
-                </div>
-            </div>
-        </div>
-    </div>`
+    var infos = JSON.parse(await getText("/files/infos/rooms.json"));
+    var container = document.getElementsByClassName("roomsSec")[0];
+    const roomCode = await getText("/files/html/room.html");
     for (let i = 0; i < 5; i++) {
-        finalCode = finalCode + roomCode
-        .replace("{num}", i+1);
+      finalCode =
+        finalCode +
+        roomCode
+          .replace("{num}", i + 1)
+          .replace("{num}", i + 1)
+          .replace("{room}", infos[i + 1]["room"]);
     }
     container.innerHTML = finalCode;
-}
-
-roomList()
+  
+    let keys = Array.from(document.getElementsByClassName("key"));
+    for (var i = 0; i < keys.length; i++) {
+      console.log(i);
+      keys[i].addEventListener("click", (function (index) {
+        return function () {
+          playsound(index);
+        };
+      })(i));
+    }
+  }
+  
+  // some music rooms idk
+  roomList();
+  
+  function playsound(num) {
+    let audio = new Audio("/files/audio/" + num + ".mp3");
+    audio.play();
+  }
