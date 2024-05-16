4a755cf96da9711e35e52139e5a26cd27c066b8f
mrdog233o5 <mr.dog233o5@gmail.com>
2024-05-16 12:26:38 +0800

    added a list for tmr

--- dest/js/ipad.js
+++ dest/js/ipad.js
@@ -1,13 +1,17 @@
 "use strict";
 
 function getCalEvents() {
-  var url = "https://script.google.com/macros/s/AKfycbxAaVuBo4lbMeQ6IGg9fjmcMKxwKAC21_N_WZcPRy-Wer_XRn52m38_Q3VpWLxpvRZt/exec?room=".concat(argv.get("room"));
+  var url = "https://script.google.com/macros/s/AKfycbwum5gPyELNHOgdA-830hj07DvRu2M2m07U_Mtgr4rckkDmfcI6SeR6Dqvxm5RnQkvu/exec?room=".concat(argv.get("room"));
   fetch(url).then(function (response) {
     return response.text();
   }).then(function (data) {
     var dataJson = JSON.parse(data);
+    console.log(dataJson);
     dataJson["events"].forEach(function (event) {
       bookerNameElements[Number(event["timePeriod"])].textContent = event["booker"];
+    });
+    dataJson["eventsTmr"].forEach(function (event) {
+      bookerNameElementsTmr[Number(event["timePeriod"])].textContent = event["booker"];
     });
   })["catch"](function (error) {
     console.error(error);
@@ -29,7 +33,11 @@ function setupDescription(roomNum) {
 var argvRaw = window.location.search;
 var argv = new URLSearchParams(argvRaw);
 var bookerNameElements = document.getElementsByClassName("bookerName");
+var bookerNameElementsTmr = document.getElementsByClassName("bookerNameTmr");
 Array.from(bookerNameElements).forEach(function (element) {
+  element.textContent = "Not booked yet";
+});
+Array.from(bookerNameElementsTmr).forEach(function (element) {
   element.textContent = "Not booked yet";
 });
 getCalEvents();
--- js/ipad.js
+++ js/ipad.js
@@ -1,5 +1,5 @@
 function getCalEvents() {
-	const url = `https://script.google.com/macros/s/AKfycbxAaVuBo4lbMeQ6IGg9fjmcMKxwKAC21_N_WZcPRy-Wer_XRn52m38_Q3VpWLxpvRZt/exec?room=${argv.get("room")}`;
+	const url = `https://script.google.com/macros/s/AKfycbwum5gPyELNHOgdA-830hj07DvRu2M2m07U_Mtgr4rckkDmfcI6SeR6Dqvxm5RnQkvu/exec?room=${argv.get("room")}`;
 	
 	fetch(url)
 	.then((response) => {
@@ -7,8 +7,12 @@ function getCalEvents() {
 	})
 	.then((data) => {
 		let dataJson = JSON.parse(data);
+		console.log(dataJson);
 		dataJson["events"].forEach((event) => {
 			bookerNameElements[Number(event["timePeriod"])].textContent = event["booker"];
+		});
+		dataJson["eventsTmr"].forEach((event) => {
+			bookerNameElementsTmr[Number(event["timePeriod"])].textContent = event["booker"];
 		});
 	})
 	.catch((error) => {
@@ -32,7 +36,11 @@ function setupDescription(roomNum) {
 var argvRaw = window.location.search;
 var argv = new URLSearchParams(argvRaw);
 var bookerNameElements = document.getElementsByClassName("bookerName");
+var bookerNameElementsTmr = document.getElementsByClassName("bookerNameTmr");
 Array.from(bookerNameElements).forEach((element) => {
+	element.textContent = "Not booked yet";
+})
+Array.from(bookerNameElementsTmr).forEach((element) => {
 	element.textContent = "Not booked yet";
 })
 getCalEvents();
